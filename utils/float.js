2fae4cd90755fbd82fe32b4c81ef66aa2fca11ba
apriliandi <apriliandi642@gmail.com>
2020-09-18 15:10:27 +0700

    add new functionality in (float)

--- utils/float.js
+++ utils/float.js
@@ -7,15 +7,39 @@ module.exports.getQueryFloat = (currentUrl) => {
    for (let [key, value] of urlParams) {
       if (!isNaN(value)) {
          const floatNumber = parseFloat(value);
-
-         if (!Number.isInteger(floatNumber)) {
-            queryFloat[key] = floatNumber;
-
-         } else {
-            queryFloat[key] = null;
-         }
+         !Number.isInteger(floatNumber) ? queryFloat[key] = floatNumber : queryFloat[key] = null;
       }
    }
 
    return queryFloat;
+}
+
+module.exports.getArrayQuaryFloat = (currentUrl) => {
+   const arrQueryFloat = [];
+   const urlParams = getParams(currentUrl);
+
+   urlParams.forEach((number) => {
+      if (!isNaN(number)) {
+         const floatNumber = parseFloat(number);
+         !Number.isInteger(floatNumber) ? arrQueryFloat.push(floatNumber) : arrQueryFloat.push(null);
+      }
+   });
+
+   return arrQueryFloat;
+}
+
+module.exports.getArrObjectFloat = (currentUrl) => {
+   let queryFloat = {};
+   const arrQueryFloat = [];
+   const urlParams = getParams(currentUrl);
+
+   for (let [key, value] of urlParams) {
+      const floatNumber = parseFloat(value);
+
+      queryFloat[key] = (!isNaN(value) && !Number.isInteger(floatNumber)) ? floatNumber : null;
+      arrQueryFloat.push(queryFloat);
+      queryFloat = {};
+   }
+
+   return arrQueryFloat;
 }
