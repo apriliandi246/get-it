09dad026e5b5cac14cd37905d55b17fc43a9d91d
parhan <apriliandi642@gmail.com>
2020-09-28 16:26:35 +0700

    refactor code (helper)

--- helper/getQuerySearch.js
+++ helper/getQuerySearch.js
@@ -1,4 +1,7 @@
-module.exports = (currentUrl) => {
-   if (currentUrl.search(/[?]/) === -1) return -1;
+module.exports = function getQuerySearch(currentUrl) {
+   if (currentUrl.search(/[?]/) === -1) {
+      return -1;
+   }
+
    return new URLSearchParams(currentUrl.slice(currentUrl.search(/[?]/)));
 }
