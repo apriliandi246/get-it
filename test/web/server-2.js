f2e231137020c83124e300a51143b6e58c0dc8c4
Farhan Apriliandi <apriliandi642@gmail.com>
2021-01-25 09:57:38 +0700

    formatting code

--- test/web/server-2.js
+++ test/web/server-2.js
@@ -1,11 +1,9 @@
-const express = require("express");
+const server = require("express")();
 const strQuerySearch = require("../../utils/string");
 const floatQuerySearch = require("../../utils/float");
 const intQuerySearch = require("../../utils/int");
-const app = express();
 
-
-app.get("/", (req, res) => {
+server.get("/", (req, res) => {
    const numIntObj = intQuerySearch("object", req.url);
    const numIntArr = intQuerySearch("array", req.url);
    const objArr = intQuerySearch("arrayObject", req.url);
@@ -15,13 +13,12 @@ app.get("/", (req, res) => {
    console.log(numIntObj);
    console.log(numIntArr);
    console.log(objArr);
-   console.log(exel)
-   console.log(str)
+   console.log(exel);
+   console.log(str);
 
    res.send("Hello, get-it");
 });
 
-
-app.listen(4000, () => {
+server.listen(4000, () => {
    console.log(`Server started on 4000`);
 });
